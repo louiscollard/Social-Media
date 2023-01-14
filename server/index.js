d64bbca5aaf040f348f624b6cf30860978d7e5bf
Louis Collard <louiscollard2@gmail.com>
2023-01-14 16:17:04 +0100

    Fix posts and add fake data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middlewares/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -68,4 +71,8 @@ const connect = () => {
 app.listen(5000, () => {
 	connect();
 	console.log("Connected!");
+
+	/* ADD DATA ONE TIME */
+	// User.insertMany(users);
+	// Post.insertMany(posts);
 });
